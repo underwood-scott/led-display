5aa2a1e1819d4ec41c9249ac9c32d66c71a4864d
Scott Underwood <scott.underwood@emeraldai.co>
2026-01-18 11:22:56 -0500

    change metro path"

--- metro_display/run.sh
+++ metro_display/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /home/sunderwood/metro_sign/rpi-metro-display/app.py /home/sunderwood/metro_sign/rpi-metro-display/log.txt 3c5f746445d44ac8bdd752265081b90e A05 1 /home/sunderwood/metro_sign/rpi-metro-display/6x10.bdf /home/sunderwood/metro_sign/rpi-metro-display/lines.json /home/sunderwood/metro_sign/rpi-metro-display/stations.json
+python3 /home/sunderwood/led-display/metro_display/app.py /home/sunderwood/led-display/metro_display/log.txt 3c5f746445d44ac8bdd752265081b90e A05 1 /home/sunderwood/led-display/metro_display/6x10.bdf /home/sunderwood/led-display/metro_display/lines.json /home/sunderwood/led-display/metro_display/stations.json

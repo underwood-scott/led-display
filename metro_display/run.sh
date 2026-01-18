06a2ae4e775ddfa5ea96acf7a15348994992f0d8
Scott Underwood <scott.underwood@emeraldai.co>
2026-01-18 11:26:03 -0500

    update fonts path

--- metro_display/run.sh
+++ metro_display/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /home/sunderwood/led-display/metro_display/app.py /home/sunderwood/led-display/metro_display/log.txt 3c5f746445d44ac8bdd752265081b90e A05 1 /home/sunderwood/led-display/metro_display/6x10.bdf /home/sunderwood/led-display/metro_display/lines.json /home/sunderwood/led-display/metro_display/stations.json
+python3 /home/sunderwood/led-display/metro_display/app.py /home/sunderwood/led-display/metro_display/log.txt 3c5f746445d44ac8bdd752265081b90e A05 1 /home/sunderwood/led-display/rpi-rgb-led-matrix/fonts/6x10.bdf /home/sunderwood/led-display/metro_display/lines.json /home/sunderwood/led-display/metro_display/stations.json
